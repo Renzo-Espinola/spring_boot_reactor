c650454591df1ec846bb8203ff1b74fbecd31203
Renzo Espinola <renzo.espinola87@gmail.com>
2024-08-13 00:55:54 -0300

    Se agrega map, filter, manejo de logs.

--- src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,13 +1,18 @@
 package com.bolsadeideas.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import models.Usuario;
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
@@ -15,10 +20,28 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux nombres = Flux.just("Renzo", "Tatiana", "Athena", "Michu")
-							.doOnNext(System.out::println);
-		nombres.subscribe();
-		
+		Flux<Usuario> nombres = Flux.just("Renzo Espinola", "Tatiana Rios", "Chucky Espinola", "Athena Espinola", "Michu Espinola", "Bruce Lee", "Bruce Willis")
+				.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+				.filter(usuario -> "Bruce".equalsIgnoreCase(usuario.getNombre()))
+				.doOnNext(usuario -> {
+					if (usuario == null) {
+						throw new RuntimeException("Nombres no pueden ser vacios");
+					}
+					System.out.println(usuario.getNombre().concat(" ").concat(usuario.getApellido()));
+				}).map(usuario -> {
+					usuario.setNombre(usuario.getNombre().toLowerCase());
+					return usuario;
+				});
+
+		nombres.subscribe(e -> log.info(e.toString()), error -> log.error(((Throwable) error).getMessage()),
+				new Runnable() {
+					@Override
+					public void run() {
+						log.info("Ha Finalizado la ejecucion del observable con exito!");
+
+					}
+				});
+
 	}
 
 }
